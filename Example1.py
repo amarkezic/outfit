80022838893f7947e5c91a0285c9ab80922e9bec
Aljaž Markežič <aljaz.markezic@gmail.com>
2018-09-26 22:22:28 +0200

    added gitignore

--- Example1.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from PySide2.QtWidgets import QApplication, QLabel
-
-if __name__ == '__main__':
-    app = QApplication([])
-    label = QLabel("Qt for Python !")
-    label.show()
-    sys.exit(app.exec_())
--- Example2.py
+++
@@ -1,6 +0,0 @@
-# Event handling
-
-import sys
-from PySide2.QtWidgets import QApplication, QLabel, QLineEdit
-from PySide2.QtWidgets import QDialog, QPushButton, QVBoxLayout
-
